913dc18f757730a926029256cc8a626154674fe1
Daniel Sahlberg <dsahlberg@apache.org>
2024-11-24 23:32:14 +0000

    Output the dump in repr() format instead of trying to decode to utf-8, which would fail if a dump file contains non-utf-8 data.
    * subversion/tests/cmdline/svntest/verify.py
      (compare_dump_files): replace X.decode('utf-8') with repr(X)
    
    Suggested by: danielsh
    https://lists.apache.org/thread/nondg6n2nydpbc2grf4j9cztmrkdd14s
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1922064 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svntest/verify.py
+++ subversion/tests/cmdline/svntest/verify.py
@@ -852,7 +852,8 @@ def compare_dump_files(label_expected, label_actual,
     print('DIFF of raw dumpfiles (including expected differences)')
     print('--- ' + (label_expected or 'expected'))
     print('+++ ' + (label_actual or 'actual'))
-    print(''.join(ndiff([line.decode('utf-8') for line in expected], [line.decode('utf-8') for line in actual])))
+    print(''.join(ndiff([repr(line) for line in expected], 
+                        [repr(line) for line in actual])))
     raise svntest.Failure('DIFF of parsed dumpfiles (ignoring expected differences)\n'
                           + '\n'.join(ndiff(
           pprint.pformat(parsed_expected).splitlines(),
