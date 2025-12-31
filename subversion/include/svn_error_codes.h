d35db3e4c377ab8ae61ef2222a5467cc3d4fbf77
Timofei Zhakov <rinrab@apache.org>
2025-12-31 21:43:19 +0000

    Add checksum implementation based on BCrypt (which is a build-in cryptography provider on Windows).
    Unlike OpenSSL, it's not comparable to the speed of light but it still earns
    its place in the supersonic fleet. Please refer to results of my measurements
    bellow:
    
    [[[
    APR (win32)     0.11 GB/s   (baseline)
    APR (Linux)     0.16 GB/s
    BCrypt          0.54 GB/s   (added in this revision)
    OpenSSL (Linux) 1.38 GB/s
    OpenSSL (win32) 1.51 GB/s
    ]]]
    
    * CMakeLists.txt
      (SVN_CHECKSUM_BACKEND): Mention new option.
      (bcrypt): Implement setup for this module.
    
    * subversion/include/svn_error_codes.h
      (SVN_ERR_BCRYPT): New error code.
    
    * subversion/libsvn_subr/checksum_bcrypt.c: Implement new module.
      (handle_error): New function.
      (algorithm_state_t): New structure.
      (md5, sha1): Declare algorithm states.
      (algorithm_init): New function (sorry for the use of some bird language).
      (bcrypt_ctx_t,
       bcrypt_ctx_cleanup,
       bcrypt_ctx_init,
       bcrypt_ctx_update,
       bcrypt_ctx_final,
       bcrypt_checksum): New structure+methods to work with it.
      (svn_checksum__md5,
       svn_checksum__md5_ctx_t,
       svn_checksum__md5_ctx_create,
       svn_checksum__md5_ctx_reset,
       svn_checksum__md5_ctx_update,
       svn_checksum__md5_ctx_final,
       svn_checksum__sha1,
       svn_checksum__sha1_ctx_t,
       svn_checksum__sha1_ctx_create,
       svn_checksum__sha1_ctx_reset,
       svn_checksum__sha1_ctx_update,
       svn_checksum__sha1_ctx_final): Implement the module API.
    
    ### Note: Future me has to fix that part where checksum backend is being set
        up by the 'openssl' target in build system, because it doesn't make that
        much sense now.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1931019 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error_codes.h
+++ subversion/include/svn_error_codes.h
@@ -1518,6 +1518,11 @@ SVN_ERROR_START
              SVN_ERR_MISC_CATEGORY_START + 47,
              "Could not canonicalize path or URI")
 
+  /** @since New in 1.16.  */
+  SVN_ERRDEF(SVN_ERR_BCRYPT,
+             SVN_ERR_MISC_CATEGORY_START + 48,
+             "BCrypt method failed.")
+
   /* command-line client errors */
 
   SVN_ERRDEF(SVN_ERR_CL_ARG_PARSING_ERROR,
