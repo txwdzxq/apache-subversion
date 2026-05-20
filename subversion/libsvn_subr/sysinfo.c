b519e8ce4e2bcffa344b8d000250f12e23de04c4
Ivan Zhakov <ivan@apache.org>
2026-05-20 15:07:02 +0000

    Make `svn --version -v` report actual character encoding used on Windows.
    Currently Windows has platform specific code for character set conversion.
    
    On Windows CP_THREAD_ACP used as "locale encoding", while
    SVN_APR_LOCALE_CHARSET is used on non-Windows platforms.
    
    * subversion/include/private/svn_utf_private.h
      (svn_utf__locale_encoding): New function declaration.
    
    * subversion/libsvn_subr/sysinfo.c
      (): Remove #include <apr_portable.h>
      (svn_sysinfo__character_encoding): Use svn_utf__locale_encoding()
       instead of apr_os_locale_encoding().
    
    * subversion/libsvn_subr/utf.c
      (svn_utf__locale_encoding): New. Use
       svn_subr__win32_xlate_locale_encoding() on Windows and
       apr_os_locale_encoding() on other platforms.
     
    * subversion/libsvn_subr/win32_xlate.c
      (svn_subr__win32_xlate_locale_encoding): New. Use GetCPInfoExW()
       to get actual codepage for CP_THREAD_ACP.
    
    * subversion/libsvn_subr/win32_xlate.h
      (svn_subr__win32_xlate_locale_encoding): New function declaration.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1934437 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/sysinfo.c
+++ subversion/libsvn_subr/sysinfo.c
@@ -35,7 +35,6 @@
 #include <apr_thread_proc.h>
 #include <apr_version.h>
 #include <apu_version.h>
-#include <apr_portable.h>       /* for apr_os_locale_encoding() */
 
 #include "svn_pools.h"
 #include "svn_ctype.h"
@@ -141,7 +140,7 @@ svn_sysinfo__release_name(apr_pool_t *pool)
 const char *
 svn_sysinfo__character_encoding(apr_pool_t *pool)
 {
-  return apr_os_locale_encoding(pool);
+  return svn_utf__locale_encoding(pool);
 }
 
 const apr_array_header_t *
